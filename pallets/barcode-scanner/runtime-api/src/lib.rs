506440e84f9b3c761a2b1ace7efa327ea2e8ea86
ayushmishra2005 <ayushmishra2005@gmail.com>
2021-05-09 17:08:30 +0530

    corrected formatting

--- pallets/barcode-scanner/runtime-api/src/lib.rs
+++ pallets/barcode-scanner/runtime-api/src/lib.rs
@@ -4,8 +4,8 @@ use codec::Codec;
 sp_api::decl_runtime_apis! {
     /// The API to verify barcode
     pub trait BarcodeScannerApi<Hash> where
-		Hash: Codec, {
-		/// Verify barcode
+        Hash: Codec, {
+        /// Verify barcode
         fn is_valid_barcode(barcode: Hash) -> bool;
     }
 }
--- pallets/barcode-scanner/src/banchmarking.rs
+++ pallets/barcode-scanner/src/banchmarking.rs
@@ -1,39 +1,39 @@
 #![cfg(feature = "runtime-benchmarks")]
 
 use super::*;
-use sp_std::prelude::*;
-use frame_system::{RawOrigin, EventRecord};
 use frame_benchmarking::{benchmarks, whitelisted_caller};
+use frame_system::{EventRecord, RawOrigin};
+use sp_std::prelude::*;
 
 fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
-	let events = frame_system::Module::<T>::events();
-	let system_event: <T as frame_system::Config>::Event = generic_event.into();
-	// compare to the last event record
-	let EventRecord { event, .. } = &events[events.len() - 1];
-	assert_eq!(event, &system_event);
+    let events = frame_system::Module::<T>::events();
+    let system_event: <T as frame_system::Config>::Event = generic_event.into();
+    // compare to the last event record
+    let EventRecord { event, .. } = &events[events.len() - 1];
+    assert_eq!(event, &system_event);
 }
 
 benchmarks! {
-	add_product {
-		let caller: T::AccountId = whitelisted_caller();
-		let barcode: T::Hash = T::Hash::default();
-		let id: T::Hash = T::Hash::default();
-	}: _(RawOrigin::Signed(caller.clone()), barcode, "Test".encode(), id)
-	verify {
-		assert_last_event::<T>(Event::<T>::ProductInformationStored(caller, barcode).into());
-	}
+    add_product {
+        let caller: T::AccountId = whitelisted_caller();
+        let barcode: T::Hash = T::Hash::default();
+        let id: T::Hash = T::Hash::default();
+    }: _(RawOrigin::Signed(caller.clone()), barcode, "Test".encode(), id)
+    verify {
+        assert_last_event::<T>(Event::<T>::ProductInformationStored(caller, barcode).into());
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use frame_support::assert_ok;
-	use crate::mock::{new_test_ext, Test};
-	use super::*;
+    use super::*;
+    use crate::mock::{new_test_ext, Test};
+    use frame_support::assert_ok;
 
-	#[test]
-	fn test_benchmarks() {
-		new_test_ext().execute_with(|| {
-			assert_ok!(test_benchmark_add_product::<Test>());
-		});
-	}
+    #[test]
+    fn test_benchmarks() {
+        new_test_ext().execute_with(|| {
+            assert_ok!(test_benchmark_add_product::<Test>());
+        });
+    }
 }
